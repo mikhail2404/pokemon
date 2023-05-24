c173d5f4d9238ffe068e3647e144970f59ccf1b4
Mikhail <mike@drupfan.com>
2023-05-24 20:29:58 +0300

    HEader changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,15 @@
 
 import React from 'react';
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 
 const Header: React.FC = () => {
+    const {query: {name}} = useRouter()
     return (
         <header className="h-10v bg-black  flex  items-center justify-center">
             <Link href="/">
-                <h1 className="text-6xl  text-amber-400">Pokedex</h1>
+                <h1 className="text-6xl  text-amber-400 capitalize">{name ? name : 'Pokedex'}</h1>
             </Link>
         </header>
     );
